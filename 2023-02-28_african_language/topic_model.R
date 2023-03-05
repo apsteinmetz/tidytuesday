911b7f7514c3957255559c73f8b54e4555cb157b
Art Steinmetz <apsteinmetz@yahoo.com>
2023-03-05 12:06:53 -0500

    working random forest model

--- 2023-02-28_african_language/topic_model.R
+++ 2023-02-28_african_language/topic_model.R
@@ -4,39 +4,45 @@ library(tidytext)
 library(tidymodels)
 library(tm)
 
-
+setwd("2023-02-28_african_language")
 load("data/afrisenti_translated.rdata")
 
-my_stop_words = tibble(word = c("http","https","dey","de","al","url",
-                                "t.co","rt","user","users",
+my_stop_words = tibble(word = c("http","https","dey","de","al","url","na",
+                                "t.co","rt","user","users","wey","don",
                                 as.character(1:100)))
 
 tokens <- afrisenti_translated %>%  
   select(tweet_num,assigned_long,translatedText,label) %>% 
-  unnest_tokens(word,translatedText )
+  unnest_tokens(word,translatedText )  |> 
+  rowid_to_column(var="word_num")
+
 
 not_words_rows <- tokens |> 
-  rowid_to_column(var="word_num") |> 
   filter(word =="not") |> 
   mutate(word_num = word_num  + 1) |> 
   pull(word_num)
 
-
-
-
-
-tokens %>% 
+tokens <- tokens %>% 
+  # create negated terms
+  mutate(word = ifelse(word_num %in% not_words_rows,paste0("not_",word),word)) |> 
   anti_join(stop_words) |> 
   anti_join(my_stop_words)
 
 
 # find most common words
+
+# word list size will be critical
+# full set will be wasteful and slow
+# one author suggested 2000
+
+word_count <- 20
+
 chosen_words <- tokens |> 
   ungroup() |> 
   select(word) |> 
   count(word) |> 
   arrange(desc(n)) |> 
-  slice_max(order_by = n,n=200)
+  slice_max(order_by = n,n=word_count)
 
 
 tweet_dtm <- tokens |> 
@@ -52,40 +58,15 @@ temp1 <- afrisenti_translated |>
 
 
 temp2 <- tidy(tweet_dtm) |> 
-  pivot_wider(names_from = term,values_from = count) |> 
-  right_join(temp)
-
-left_join(temp1,temp2)
-
-
-randomForest::randomForest(label)
+  pivot_wider(names_from = term,values_from = count) |>  
+  mutate(across(everything(), ~replace_na(.x, 0)))
 
 
 
-col_set <-   c("#952038","#F7C608","#319400")
-  
-# feelings about world powers
-north_countries <- c("usa","europe","russia","china")
-tokens |>
-  filter(word %in% north_countries) |> 
-  mutate(Area = str_to_title(word)) |> 
-  mutate(Area = str_replace(Area,"Usa","USA")) |> 
-  rename(Sentiment = label) |> 
-  ggplot(aes(Area,n,fill = Sentiment)) + 
-  geom_col(position = "dodge") + 
-  scale_fill_manual(values =  col_set) + 
-  labs(title = 'Q:How Does Africa Feel About the Global North?\nA: Not great, but Tweets mentioning Russia are mostly "neutral"',
-       subtitle = "WARNING: Sample size is not large. Other caveats apply.",
-       y = "Tweet Count",
-       caption = "Source: Afrisenti data set") +
-  coord_flip() +
-  annotate("text",x = 2.5,y = 70,label = "Alternate names for\nareas are combined.\ne.g. EU = Europe") +
-  theme(text = element_text(family = "dm"),
-      plot.background = element_rect(fill = "#FDECCD", color = NA),
-      legend.background = element_blank(),
-      axis.ticks = element_blank(),
-      axis.text.y = element_text(face = "bold"),
-      plot.subtitle = element_text(face = "italic"),
-      panel.background = element_blank())
+dtmm <- right_join(temp1,temp2) |> 
+  select(-document)
+
+
+randomForest::randomForest(label ~.,data=dtmm)
 
 
